0b11fa139435497373b4bc784fdf51e8afc3af51
hakimbouharb <6869@holbertonstudents.com>
2024-01-11 09:14:53 -0500

    $[Dr.Wise]@(Kali)//:{Holberton}$

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+!/usr/bin/env python3
+"""
+1. Basic annotations - concat
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """function concat that takes "str1" and "str2"
+    and returns a concatenated string"""
+    return str1 + str2
