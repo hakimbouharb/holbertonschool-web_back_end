b905169db59900bd0888e587c7a8d5a038675251
hakimbouharb <6869@holbertonstudents.com>
2024-01-11 09:13:36 -0500

    $[Dr.Wise]@(Kali)//:{Holberton}$

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+0. Basic annotations - add
+"""
+
+
+def add(a: float, b: float) -> float:
+    """function add that takes "a" and "b"
+    and returns their sum as a float"""
+    return a + b
