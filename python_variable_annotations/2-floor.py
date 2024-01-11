97b9b284f5500bc76529296c6d0983eda11f9ff2
hakimbouharb <6869@holbertonstudents.com>
2024-01-11 09:17:21 -0500

    $[Dr.Wise]@(Kali)//:{Holberton}$

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+2. Basic annotations - floor
+"""
+
+
+def floor(n: float) -> int:
+    """
+    floor which takes a float n as argument
+    and returns the floor of the float.
+    """
+    return int(n)
