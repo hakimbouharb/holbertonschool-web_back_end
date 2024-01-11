c134c23db6ac85ac3c0eadeda34e4e6e717247b4
hakimbouharb <6869@holbertonstudents.com>
2024-01-11 09:19:32 -0500

    $[Dr.Wise]@(Kali)//:{Holberton}$

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+4. Define variables
+"""
+
+"""
+    Define and annotate the following variables
+    with the specified values:
+"""
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
