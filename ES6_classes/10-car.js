1757ad8e5151672f37c2629972b5a607495c09fc
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 <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>
2024-02-10 14:17:45 -0500

    $[Dr.Wise]@(Kali)//:{Holberton}$

--- ES6_classes/10-car.js
+++ ES6_classes/10-car.js
@@ -1,16 +1,14 @@
+const _brand = Symbol('brand');
+const _motor = Symbol('motor');
+const _color = Symbol('color');
 export default class Car {
   constructor(brand, motor, color) {
-    this._brand = brand;
-    this._motor = motor;
-    this._color = color;
-  }
-
-  static get [Symbol.species]() {
-    return this;
+    this[_brand] = brand;
+    this[_motor] = motor;
+    this[_color] = color;
   }
 
   cloneCar() {
-    const Species = this.constructor[Symbol.species];
-    return new Species();
+    return new this.constructor(this[_brand], this[_motor], this[_color]);
   }
 }
