ce1dddb5a171ad1fd8d459c129a672e182d5784d
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 <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>
2024-02-10 14:14:53 -0500

    $[Dr.Wise]@(Kali)//:{Holberton}$

--- ES6_classes/9-hoisting.js
+++ ES6_classes/9-hoisting.js
@@ -32,7 +32,7 @@ export class StudentHolberton {
   }
 
   get fullStudentDescription() {
-    return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
+    return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass._location}`;
   }
 }
 
@@ -43,5 +43,4 @@ const student4 = new StudentHolberton('Donald', 'Bush', class2019);
 const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
 
 const listOfStudents = [student1, student2, student3, student4, student5];
-
 export default listOfStudents;
